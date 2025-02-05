2f76d9cf0c7c25049189ff65656926eb688e3fa3
Ciaran Otter <ciaranjotter@gmail.com>
2025-02-05 16:06:50 +0200

    removing webhook in the meantime

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,14 +1,14 @@
-import { createClient } from '@supabase/supabase-js'
-import { env } from '$env/dynamic/private'
-import type {Handle} from '@sveltejs/kit'
+// import { createClient } from '@supabase/supabase-js'
+// import { env } from '$env/dynamic/private'
+// import type {Handle} from '@sveltejs/kit'
 
-export const supabase = createClient(
-    env.VITE_SUPABASE_URL,
-    env.VITE_SUPABASE_ANON_KEY
-)
+// export const supabase = createClient(
+//     env.VITE_SUPABASE_URL,
+//     env.VITE_SUPABASE_ANON_KEY
+// )
 
-export const handle: Handle =  async ({ event, resolve }) => {
-    event.locals.supabase = supabase
-    console.log("running web hook")
-    return await resolve(event)
-}
+// export const handle: Handle =  async ({ event, resolve }) => {
+//     event.locals.supabase = supabase
+//     console.log("running web hook")
+//     return await resolve(event)
+// }
