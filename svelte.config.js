7b05203091e1dc6bde69380b2f02011cc9d13c7c
Ciaran Otter <ciaranjotter@gmail.com>
2025-02-05 16:20:45 +0200

    configured the prerendering of the site

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,37 @@
 import adapter from '@sveltejs/adapter-static';
 
+const config = {
+    kit: {
+      prerender: {
+        handleHttpError: ({ path, referrer, message }) => {
+          // Ignore specific paths or handle differently
+          if (path === '/') {
+            return;
+          }
+          throw new Error(message);
+        }
+      }
+    }
+  };
+
 export default {
     kit: {
-        adapter: adapter(),
-        paths: {
-            base: process.env.NODE_ENV === 'production' ? '/otternonesenses-co-za.github.io' : ''
-        }
+        adapter: adapter({
+            fallback: "200.html"
+        }),
+        prerender: {
+			handleHttpError: ({ path, referrer, message }) => {
+				// ignore deliberate link to shiny 404 page
+				if (path === '/') {
+					return;
+				}
+
+				// otherwise fail the build
+				throw new Error(message);
+			}
+		}
+        // paths: {
+        //     base: process.env.NODE_ENV === 'production' ? '/otternonesenses-co-za.github.io' : ''
+        // },
     }
 };
