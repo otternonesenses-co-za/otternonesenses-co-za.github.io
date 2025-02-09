e1c2643e11dc7733a834bc41782e534fdaebeb07
Ciaran Otter <ciaranjotter@gmail.com>
2025-02-09 16:43:36 +0200

    removing old out-dated comment from ackground

--- src/components/background.svelte
+++ src/components/background.svelte
@@ -2,21 +2,6 @@
     import { onMount } from "svelte";
     import * as d3 from "d3";
     import { lightPosition } from "three/src/nodes/TSL.js";
-    /*
-     * A speed-improved perlin and simplex noise algorithms for 2D.
-     *
-     * Based on example code by Stefan Gustavson (stegu@itn.liu.se).
-     * Optimisations by Peter Eastman (peastman@drizzle.stanford.edu).
-     * Better rank ordering method by Stefan Gustavson in 2012.
-     * Converted to Javascript by Joseph Gentle.
-     *
-     * Version 2012-03-09
-     *
-     * This code was placed in the public domain by its original author,
-     * Stefan Gustavson. You may use it as you see fit, but
-     * attribution is appreciated.
-     *
-     */
 
     function updateCanvas(canvas) {
         canvas.style.width = window.innerWidth;
